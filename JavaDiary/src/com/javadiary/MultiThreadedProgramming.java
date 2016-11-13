ea0bafdbeeeaadd93e635e7243969bf9e33c3a98
Roberto Ambrosio <roberto@ambrosio.ca>
2016-11-14 00:53:51 +0800

    Completed the Operators example

---
+++ JavaDiary/src/com/javadiary/MultiThreadedProgramming.java
@@ -0,0 +1,10 @@
+package com.javadiary;
+
+public class MultiThreadedProgramming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- JavaDiary/src/com/javadiary/Operators.java
+++ JavaDiary/src/com/javadiary/Operators.java
@@ -3,7 +3,18 @@ package com.javadiary;
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int ten = 10;
+		int addition = ten + ten;
+		int subtraction = ten - ten;
+		int division = ten / ten;
+		int mutiplication = ten * ten;
+		int modulus = ten % ten;
+		System.out.println("Below is the example of Java Operators");
+		System.out.println("10+10=" + addition);
+		System.out.println("10-10=" + subtraction);
+		System.out.println("10*10=" + mutiplication);
+		System.out.println("10/10=" + division);
+		System.out.println("10%10=" + modulus);
 
 	}
 
